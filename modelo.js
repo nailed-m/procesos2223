2608e6cc6f4b0ebced275add3880565f8d09fe0e
nailed-m <daniel.montealegre@alu.uclm.es>
2022-09-21 15:33:57 +0200

    commit

--- modelo.js
+++ modelo.js
@@ -1,13 +1,29 @@
 function Juego(){
 	this.partidas=[];
-	this.agregarPartida=function(nombre){
-		this.partidas.push(new Partida(nombre))
+	this.usuarios={}; //array asociativo
+	
+	this.agregarUsuario=function(nick){
+		if(!this.usuarios[nick]){
+			this.usuarios[nick])new Usuario(nick)
+		}
 	}
-	this.eliminarPartida=function(nombre){
+	
+	this.crearPartida=function(nick){
 		//TODO
+		//obtener código único
+		//crear la partida con propietario nick
+		//devolver el código
 	}
 }
 
-function Partida(nombre){
-	this.nombre=nombre;
+function Partida(){
+	this.codigo
+}
+
+function Usuario(nick, juego){
+	this.nick=nick;
+	this.juego=juego;
+	this.crearPartida=function(){
+		this.juego.crearPartida(nick)
+	}
 }
