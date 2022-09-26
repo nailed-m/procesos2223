e81034be3d9234f63a2e0ee298001c1603150a85
nailed-m <daniel.montealegre@alu.uclm.es>
2022-09-26 13:01:40 +0200

    clase 26-09-2022

--- modelo.js
+++ modelo.js
@@ -7,23 +7,47 @@ function Juego(){
 			this.usuarios[nick]=new Usuario(nick)
 		}
 	}
+	this.eliminarUsuario=function(nick){
+		delete this.usuarios[nick];
+	}
 	
 	this.crearPartida=function(nick){
-		//TODO
-		//obtener código único
-		//crear la partida con propietario nick
-		//devolver el código
+		let codigo=Date.now();
+		this.partidas[codigo]=new Partida(codigo,nick);
+		return codigo;
 	}
-}
-
-function Partida(){
-	this.codigo
 }
 
 function Usuario(nick, juego){
 	this.nick=nick;
 	this.juego=juego;
 	this.crearPartida=function(){
-		this.juego.crearPartida(nick)
+		return this.juego.crearPartida(this.nick)
+	}
+	this.unirseAPartida=function(codigo){
+		if(this.partidas[codigo]){
+			this.partidas[codigo].agregarJugador(nick);
+		}
+		else{
+			console.log("La partida no existe");
+		}
+	}
+	this.obtenerPartidas=function(){
+			let lista=[];
+			for(var key in this.partidas){
+				lista.push({"codigo":key,"owner":this.partidas[codigo].owner})
+			}
+	}
+}
+
+function Partida(codigo,nick){
+	this.codigo=codigo;
+	this.owner=nick;
+	this.jugadores=[]; //array normal o asociativo
+	this.fase="inicial";
+	this.agregarJugador=function(nick){
+		if(this.jugadores.length<2){
+			this.jugadores.push(nick);
+		}
 	}
 }
---
+++ spec/modeloSpec.js
@@ -0,0 +1,27 @@
+describe("El juego...", function() {
+    var miJuego;
+    var usr1,usr2;
+  
+    beforeEach(function() {
+        miJuego=new Juego();
+        miJuego.agregarUsuario("pepe");
+        miJuego.agregarUsuario("luis");
+        usr1=miJuego.usuarios["pepe"];
+        usr1=miJuego.usuarios["luis"];
+
+    });
+  
+    it("inicialmente", function() {
+        let lista=miJuego.obtenerPartidas();
+        expect(lista.lenght).toEqual(0);
+        expect(usr1.nick).toEqual("pepe");
+        expect(usr2.nick).toEqual("luis");
+    });
+
+    it("al crear partida", function(){
+        let codigo=usr1.crearPartida();
+        expect(miJuego.)
+    })
+  
+  });
+  
